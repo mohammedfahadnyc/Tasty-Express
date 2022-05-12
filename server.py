b364f1808fcbaa54ce8c675ad19133c6a91d6ea6
Gaurav Kuwar <gauravk@Gauravs-MacBook-Pro.local>
2022-05-12 02:02:34 -0400

    Added some home page code, a simple restraunt table, simplified search page, and basic search functionality

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import desc
 
 
 app = Flask(__name__)
@@ -14,17 +15,40 @@ class user(db.Model):
     name = db.Column(db.String(120))
     password = db.Column(db.String(80))
 
+class restaurant(db.Model):
+    rid = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    # sub_title = db.Column(db.String(120))
+    # category = db.Column(db.String(120))
+    rating = db.Column(db.Integer)
+
+    def __init__(self, name=None, rating=None):
+        self.name = name
+        self.rating = rating
 
 @app.route("/")
 def index():
-    # top 10 restraunts based on rating
     search_val = request.args.get("search_val")
-    restaurants = ({"name": f"Restaurant {str(num)}", "rating": f"{str(num % 5)}.5"} for num in range(1, 11))
 
+    # just used to populate restraunt table
+    # for num in range(1, 11):
+    #     r = restaurant(f"Restaurant {str(num)}", num % 5)
+    #     db.session.add(r)
+    #     db.session.commit()
+
+    # searchs based on if the search_val occurs in any restraunt name
     if search_val:
-        return render_template("search.html")
-
-    return render_template("home.html", restaurants=restaurants)
+        # currently just using a simple like filter
+        # I added limit of 9 to keep the page short, but it works with no limit
+        restaurants = restaurant.query.filter(restaurant.name.like(f"%{search_val}%")).limit(9).all()
+        # splits restraunts into chunks of 3
+        rest_in_row = 3
+        restaurant_by_row = [restaurants[i:i+rest_in_row] for i in range(0, len(restaurants), rest_in_row)]
+        return render_template("search.html", restaurants=restaurant_by_row)
+    else: # this leads to regular homepage
+        # top 5 restraunts based on rating
+        top_5 = restaurant.query.order_by(desc(restaurant.rating)).limit(5).all()
+        return render_template("home.html", restaurants=top_5)
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -35,7 +59,7 @@ def login():
 
         login = user.query.filter_by(username=uname, password=passw).first()
         if login is not None:
-            return render_template("home.html")
+            return redirect(url_for('index')) # goes to home page / and url also changes
     return render_template("login.html")
 
 
